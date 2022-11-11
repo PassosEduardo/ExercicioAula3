f48aaa5c57a94e6e02b76c53b96263a72a778451
PassosEduardo <dudusegui@gmail.com>
2022-11-11 14:50:52 -0300

    mudanças

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 const ligaLampada = document.getElementById('liga');
 const desligaLampada = document.getElementById('desliga');
+const quebraLampada = document.getElementById('quebra');
 const lamp = document.getElementById("lampada");
+// var estaQuebrada = false;
 
+
+function LampadaEstaQuebrada () {
+    return lamp.src.indexOf ('quebrada') > -1  
+}
 
 ligaLampada.addEventListener("click", () =>{
-        lamp.src = './img/ligada.jpg';   
+    if(!LampadaEstaQuebrada ())      
+        lamp.src = "./img/ligada.jpg"
+    
 })
 
 desligaLampada.addEventListener("click", () =>{
-    lamp.src = "./img/desligada.jpg";
+    if(!LampadaEstaQuebrada ())  
+        lamp.src = "./img/desligada.jpg";
 })
 
- 
+ quebraLampada.addEventListener("click", () =>{
+    lamp.src = "./img/quebrada.jpg";
+ })
